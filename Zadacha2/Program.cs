bde6fcbe2f00e40f993b0a531eb6dfeb3feae19d
Gromov Igor <gaz.oil.gromov@gmail.com>
2023-03-04 13:54:32 +0500

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Рекурсия!
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumInRange(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+
+    return GetSumInRange(m + 1, n) + m;
+}
+
+int start = Prompt("Write down start:");
+int end = Prompt("Write down end:");
+
+int sum = GetSumInRange(start, end);
+Console.WriteLine($"M = {start}, N = {end} -> {sum}");
