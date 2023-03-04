37b0f44ce3c4bb6f238049288a34d74ed6fbd89d
Gromov Igor <gaz.oil.gromov@gmail.com>
2023-03-04 13:50:50 +0500

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ShowNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m} ");
+    }
+
+    ShowNumbers(m + 1, n);
+}
+
+int start = Prompt("Write down start:");
+int end = Prompt("Write down end:");
+Console.Write($"M = {start}, N = {end} -> ");
+ShowNumbers(start, end);
